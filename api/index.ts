bd17e9ead576be140851733329ec2ff323bc4b8b
Falasw femi <femifalase228@gmail.com>
2026-03-17 14:52:27 +0100

    chnage node to nodejs

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import { handle } from 'hono/vercel'
 // Ensure this path is exactly correct relative to api/index.ts
 import app from '../src/index'
 
-export const config = { runtime: 'node' }
+export const config = { runtime: 'nodejs' }
 
 // Re-exporting or explicitly using the handle
 const handler = handle(app)
